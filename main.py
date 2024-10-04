cc805c8ee6b8b7e5194bfd2f471fcb06e074b4e6
FREDYK1 <fkwekukankam@gmail.com>
2024-10-04 14:15:33 +0000

    Make the Book and available methods functional

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pandas as pd
 
 
-df = pd.read_csv('hotels.csv')
+df = pd.read_csv('hotels.csv', dtype={"id": str})
+
 
 class Hotel:
     def __init__(self, hotel_id):
-        pass
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Change availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv('hotels.csv', index=False)
 
     def available(self):
-        pass
+        """Check if hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_booked):
